7d7e156477b4f8449fa297b08e9aec9cea581b08
Idan Refaeli <idanref@gmail.com>
2022-08-23 16:59:01 +0300

    Changed index.js to 1 file

--- index.js
+++ index.js
@@ -1,4 +1,29 @@
-const app = require('./app');
+const path = require('path');
+const express = require('express');
+require('./db/mongoose');
+const commentRouter = require('./routers/comment');
+const workshopRouter = require('./routers/workshop');
+
+const app = express();
+
+// Each app.use(middleware) is called every time a request is sent to the server.
+
+app.use(express.json());
+app.use(commentRouter);
+app.use(workshopRouter);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API Running');
+  });
+}
+
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
